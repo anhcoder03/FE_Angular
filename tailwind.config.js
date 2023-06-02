90a942c263c62730fe930c97f1a202bbc4f55e2e
anhcoder03 <nguyenphianh161003@gmail.com>
2023-06-02 18:06:48 +0700

    [Feature] Base Style Tailwind and Font refs: T1-22

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#be1e2d",
+        secondary: "#F2F2F2",
+      },
+    },
+  },
+  plugins: [],
+};
